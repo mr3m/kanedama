078ec5fe25ede060b9004dcaa2ada0a63b548e6e
William Hermozo <william.hermozo@protonmail.com>
2021-10-07 18:06:37 +0200

    feat: add Mansa title in Next project

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org" style={{textDecoration: "line-through"}}>Next.js!</a> <br/>Mansa&apos;s Kata test!
         </h1>
 
         <p className={styles.description}>
